ba0a620fd119be016de2a759de03fbf43bae80c1
Loh Eng Liang <lohengliang@gmail.com>
2020-02-17 17:07:26 +0800

    Updates

---
+++ precache-manifest.1380bb8d22394c98265a83cc576eac66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8f17a9f5d7045725012e6457570518",
+    "url": "/react-redux-reddit-api/index.html"
+  },
+  {
+    "revision": "47ab7765622f52e527b1",
+    "url": "/react-redux-reddit-api/static/js/2.9629c953.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-redux-reddit-api/static/js/2.9629c953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879183d63b7098d1f2b7",
+    "url": "/react-redux-reddit-api/static/js/main.9183bbca.chunk.js"
+  },
+  {
+    "revision": "fe4d295a51f066070978",
+    "url": "/react-redux-reddit-api/static/js/runtime-main.727ad71f.js"
+  }
+]);
